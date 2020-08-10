bf9637abb9134d901c3aebbc5a0a49949fd8dda5
mattcasagrande <mattdsign.mc@gmail.com>
2020-08-10 12:33:39 -0300

    Updates

---
+++ precache-manifest.c61448bf2dadf21e7a56bbe08f9b4f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19906224a2b882637094b8222fe99ab6",
+    "url": "/mattcasagrande/index.html"
+  },
+  {
+    "revision": "2c3454662298d4a66f14",
+    "url": "/mattcasagrande/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b31649a5e79a74ca55e5",
+    "url": "/mattcasagrande/static/js/2.5ffce8af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mattcasagrande/static/js/2.5ffce8af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3454662298d4a66f14",
+    "url": "/mattcasagrande/static/js/main.f887379d.chunk.js"
+  },
+  {
+    "revision": "8961b4ff40bd360c8932",
+    "url": "/mattcasagrande/static/js/runtime-main.a3679dd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mattcasagrande/static/media/logo.5d5d9eef.svg"
+  }
+]);
